bd86368f0c53e0eb457890b8421a804030fcdcd7
dmilic-foi <dmilic@foi.hr>
2018-11-12 18:11:22 +0100

    Reorganised folder structure

---
+++ server/main.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once "./Database.php";
+
+$user = "air1813";
+$password = "uv0K8QLWMo5qBnbr";
+$database = "air1813";
+$db = new Database($user, $password, $database);
+
+echo "Hello world";
