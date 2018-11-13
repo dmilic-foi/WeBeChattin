fe31836ec5f75f9c58b60f879413da4f8346f723
dmilic-foi <dmilic@foi.hr>
2018-11-13 11:57:15 +0100

    Server can receive more than one message.

--- server/main.php
+++ server/main.php
@@ -7,7 +7,7 @@ $password = "uv0K8QLWMo5qBnbr";
 $database = "air1813";
 $db = new Database($user, $password, $database);
 
-$host = $_SERVER['SERVER_ADDR'];
+$host = filter_input(INPUT_SERVER, 'SERVER_ADDR');
 $port = 51345;
 
 
@@ -15,10 +15,15 @@ $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket
 socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
 socket_listen($socket, 3) or die("Could not set up socket listener\n");
 $conn = socket_accept($socket) or die("Could not accept incoming connection\n");
-$recv = socket_read($conn, 1024) or die("Could not read received message\n");
 
-echo $recv;
+do {
+    $recv = socket_read($conn, 1024) or die("Could not read received message\n");
+    echo $recv;
+    $output = "All great.";
+    socket_write($conn, $output, strlen ($output)) or die("Could not write output\n"); 
+} while ($recv != "bye");
 
-$output = "All great.\n";
-socket_write($conn, $output, strlen ($output)) or die("Could not write output\n"); 
+socket_close($conn);
+socket_close($socket);
 
+echo "Sockets closed.";
