d6158bf80041582eacb28d78e124b38e2c6b84d3
dmilic-foi <dmilic@foi.hr>
2018-11-13 12:38:55 +0100

    Server can handle multiple connections and read messages.

--- server/main.php
+++ server/main.php
@@ -14,16 +14,41 @@ $port = 51345;
 $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
 socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
 socket_listen($socket, 3) or die("Could not set up socket listener\n");
-$conn = socket_accept($socket) or die("Could not accept incoming connection\n");
+
+$clients = array();
 
 do {
-    $recv = socket_read($conn, 1024) or die("Could not read received message\n");
-    echo $recv;
-    $output = "All great.";
-    socket_write($conn, $output, strlen ($output)) or die("Could not write output\n"); 
-} while ($recv != "bye");
+    $read = array_merge(array($socket), $clients);
+    $write = NULL;
+    $except = NULL;
+    $tv_sec = 5;
+    if (!socket_select($read, $write, $except, $tv_sec)) {
+        continue;
+    }
+    
+    if (in_array($socket, $read)) {
+        $conn = socket_accept($socket) or die("Could not accept incoming connection\n");
+        $clients[] = $conn;
+        $key = array_keys($clients, $conn);
+        $msg = "All good\n";
+        socket_write($conn, $msg, strlen($msg));
+    }
+    
+    foreach ($clients as $key => $client) {
+        if (in_array($client, $read)) {
+            $recv = socket_read($client, 2048) or die("Could not read received message\n");
+            echo "Received: " . $recv . "\n";
+            if ($recv == 'bye') {
+                unset($clients[$key]);
+                socket_close($client);
+                break;
+            }
+            $answer = "Client {$key}: $recv.\n";
+            echo "Answer: " . $answer . "\n";
+            socket_write($client, $answer, strlen($answer));
+        }
+    }
+    
+} while(1);
 
-socket_close($conn);
 socket_close($socket);
-
-echo "Sockets closed.";
