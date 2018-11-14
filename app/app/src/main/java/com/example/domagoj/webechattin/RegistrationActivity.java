dffe4755c892c7d125bdc3ced3052261ebf5f0ca
Andrea Kljaić <akljaicka@gmail.com>
2018-11-14 09:43:47 +0100

    Created activities and layouts
    Created activities and layouts needed for registration and tabbed
    activity

---
+++ app/app/src/main/java/com/example/domagoj/webechattin/RegistrationActivity.java
@@ -0,0 +1,7 @@
+package com.example.domagoj.webechattin;
+
+import android.app.Activity;
+
+public class RegistrationActivity extends Activity {
+
+}
---
+++ app/app/src/main/java/com/example/domagoj/webechattin/TabChat.java
@@ -0,0 +1,4 @@
+package com.example.domagoj.webechattin;
+
+public class TabChat {
+}
