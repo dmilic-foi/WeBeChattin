4b1089444b8654ffe7c5e11522f5e8579ccb94ee
dmilic-foi <dmilic@foi.hr>
2018-11-15 17:05:22 +0100

    Refactored client code

--- app/app/src/main/java/com/example/domagoj/webechattin/MainActivity.java
+++ app/app/src/main/java/com/example/domagoj/webechattin/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.domagoj.webechattin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.example.domagoj.webechattin.chat.Client;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -22,61 +25,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Log.d("SOCKETS: ", "Creating socket...");
-
-                Socket sock;
-
-                try {
-                    sock = new Socket("161.53.120.171", 51345);
-                    Log.d("SOCKETS: ", "Socket created.");
-                } catch(Exception e) {
-                    Log.d("SOCKETS: ", "Socket creation failed."+e);
-                    return;
-                }
-                BufferedReader in;
-                PrintWriter out;
-                try {
-                    in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-                    out = new PrintWriter(sock.getOutputStream(), true);
-                    Log.d("SOCKETS: ", "Created socket streams.");
-                } catch(IOException e) {
-                    Log.d("SOCKETS: ", "Cant create socket streams");
-                    return;
-                }
-
-
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("phone_number", "0987654321");
-                    json.put("chat", "0");
-                    json.put("message", "Hello world");
-                    Log.d("SOCKETS: ", "Encoded to JSON");
-                } catch(JSONException e) {
-                    Log.d("SOCKETS: ", "Encodint to JSON failed.");
-                    return;
-                }
-
-                try {
-                    Log.d("SOCKETS: ", in.readLine());
-                } catch(IOException e) {
-                    Log.d("SOCKETS: ", "Failed receiving first message.");
-                    return;
-                }
-
-                while(true) {
-                    try {
-                        out.println(json);
-                        Log.d("SOCKETS: ", in.readLine());
-                    } catch(IOException e) {
-                        Log.d("SOCKETS: ", "Failed sending/receiving message.");
-                        return;
-                    }
-                }
-            }
-        }).start();
+        Client client = new Client("161.53.120.171", 51345);
+        client.execute();
 
     }
 }
